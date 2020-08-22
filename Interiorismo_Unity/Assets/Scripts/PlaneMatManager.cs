55ebe9516ffc485e6ec95285fe9ca27b0e9ab2cc
Sukriti Dawar <sukritidawar@gmail.com>
2020-08-22 19:36:50 +0530

    Added project files

---
+++ Interiorismo_Unity/Assets/Scripts/PlaneMatManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlaneMatManager : MonoBehaviour
+{
+    public Material PlaneMat;
+    public Button[] planetextbuttons;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        foreach(var b in planetextbuttons)
+        {
+            Texture tex = b.transform.Find("Mask/RawImage").GetComponent<RawImage>().texture;
+            b.onClick.AddListener(()=>OnClickButton(tex));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnClickButton(Texture tex)
+    {
+        PlaneMat.mainTexture = tex;
+    }
+}
